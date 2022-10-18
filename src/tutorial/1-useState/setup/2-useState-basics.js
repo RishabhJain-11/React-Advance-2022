766013ccd22206e816a1cfa89cf2739433c4aaf3
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-18 17:22:30 +0530

    ..

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
+/**
+ * Hooks Basics
+ * use
+ * component must be uppercase
+ * must be in the function/component body
+ * cannot call conditionally
+ */
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    setText("Hello World");
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>
+      Change Title
+    </button>
+  </React.Fragment>;
 };
 
 export default UseStateBasics;
