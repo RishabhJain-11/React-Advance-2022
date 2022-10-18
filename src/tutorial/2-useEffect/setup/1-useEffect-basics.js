a94a090ba03b63fb2c89b77471d95ba6712b8d01
Rishabh Jain <jainrishabh71011@gmail.com>
2022-10-18 22:00:42 +0530

    ..

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,23 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("Call Effect");
+  });
+  console.log('Render Component');
+  return <>
+    <h1>{value}</h1>
+    <button className='btn' onClick={() => setValue(value + 1)}>
+      Click Me
+    </button>
+  </>;
 };
 
 export default UseEffectBasics;
+
+// 70 rallies - 35 days
+// 70 Jansabha - 35 days
+// 10 : 140
